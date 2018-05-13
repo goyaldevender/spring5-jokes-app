7745ed5db69aab06e7528d5df85dd27866ba4e96
Devender Goyal <goyaldevender@users.noreply.github.com>
2018-05-13 15:10:44 +0530

    initial commit

---
+++ src/main/java/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,29 @@
+package springframework.joke.controllers;
+
+import springframework.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Devender on 5/13/18.
+ */
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/springframework/joke/services/JokeService.java
@@ -0,0 +1,9 @@
+package springframework.joke.services;
+
+/**
+ * Created by Devender on 5/13/18.
+ */
+public interface JokeService {
+
+    String getJoke();
+}
